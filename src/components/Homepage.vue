15632444b8488305afe2073832d79eec45819114
Jordan Hudgens <jordan@devcamp.com>
2018-05-17 17:07:37 -0500

    Installed axios and communicated with daily smarty API

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -3,14 +3,25 @@
     <h1>Homepage</h1>
 
     <input type="text" @keyup.enter="submitQuery">
+
+    <pre>
+        {{ recentPosts }}
+    </pre>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Homepage",
   data() {
-    return {};
+    return {
+      recentPosts: []
+    };
+  },
+  beforeMount() {
+    this.getRecentPosts();
   },
   methods: {
     submitQuery(evt) {
@@ -18,6 +29,17 @@ export default {
         name: "SearchResults",
         params: { query: evt.target.value }
       });
+    },
+    getRecentPosts() {
+      axios
+        .get("https://api.dailysmarty.com/posts")
+        .then(response => {
+          this.recentPosts.push(...response.data.posts);
+          console.log(response.data.posts);
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
   }
 };
