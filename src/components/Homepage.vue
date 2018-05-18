dbf6801165265ed3be417ffba3b76d0c41b35211
Jordan Hudgens <jordan@devcamp.com>
2018-05-17 19:07:47 -0500

    Implemented styles for homepage

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>
-    <h1>Homepage</h1>
+   <div class="container container-homepage">
+        <div class="homepage-logo">
+            <img src="@/assets/ds_circle_logo.png" alt="">
+        </div>
 
-    <input type="text" @keyup.enter="submitQuery">
+        <div class="search-bar homepage-search-bar">
+            <input type="text" placeholder="&#xf002; Search DailySmarty" @keyup.enter="submitQuery">
+            <p>Press return to search</p>
+        </div>
 
-    <div v-for="post in recentPosts" :key="post.id">
-        {{ post.title }}
+        <div class="recent-posts-wrapper">
+            <div class="recent-posts-heading">
+                Recent Posts
+            </div>
+
+            <div class="recent-posts">
+                <div v-for="post in recentPosts" :key="post.id" class="recent-post">
+                    <div class="recent-post-title">
+                        <a :href="post.url_for_post" target="_blank">
+                        {{ post.title }}
+                        </a>
+                    </div>
+
+                    <div class="recent-post-category">
+                        {{ post.associated_topics[0] }}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
